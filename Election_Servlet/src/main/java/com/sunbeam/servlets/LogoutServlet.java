46db7bbfc79afcda776fe5dca76be2609c580337
vrushabh <vrushabhkumatale1999@gmail.com>
2024-01-23 19:05:33 +0530

    logout

---
+++ Election_Servlet/src/main/java/com/sunbeam/servlets/LogoutServlet.java
@@ -0,0 +1,36 @@
+package com.sunbeam.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+				out.println("<head>");
+				out.println("<title>Logout</title>");
+				out.println("</head>");
+				out.println("<body>");
+				out.println("<h5>Thank you for voting.</h5>");
+				out.println("<a href='index.html'>Login Again</a>");
+				out.println("</body>");
+				out.println("</html>");
+	}
+
+}
